88769acf1b7b9f47a08613ad778f8f6f58364192
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-05-09 01:44:53 -0300

    primeiro EXEC este script para gerar o CHROMA

--- 4_Chat_with_Your_Documents-OpenAI_with_LangChain/init_vectorstore.py
+++ 4_Chat_with_Your_Documents-OpenAI_with_LangChain/init_vectorstore.py
@@ -14,8 +14,14 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
+# Isto é quando usas o arquivo .env:
+from dotenv import load_dotenv
 import os
-os.environ["OPENAI_API_KEY"] = "CHAVE API"
+#print('Carregando a minha chave Key: ', load_dotenv())
+load_dotenv()
+Eddy_API_KEY_OpenAI = os.environ['OPENAI_API_KEY'] 
+Eddy_API_KEY_HuggingFace = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+
 
 with open("/home/eddygiusepe/1_Eddy_Giusepe/3_estudando_LLMs/Large_Language_Models_LLMs/4_Chat_with_Your_Documents-OpenAI_with_LangChain/RESUMINHO_estado_da_Uniao.txt") as f:
     state_of_the_union = f.read()
