89047b9d10e7a638dd58e4859f5ea310ac3e0262
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-05-18 15:28:00 -0300

    Usando Agentes do LangChain

--- LangChain_with_Sam-Witteveen/8_Agentes_JoiningTools_and_Chains_with_Decisions.py
+++ LangChain_with_Sam-Witteveen/8_Agentes_JoiningTools_and_Chains_with_Decisions.py
@@ -1,5 +1,17 @@
 """
 Data Scientist.: PhD.Eddy Giusepe Chirinos Isidro
+
+
+LangChain Agents = Tools + Chains
+=================================
+
+Neste script, examinamos os agentes LangChain e como eles permitem que você use várias ferramentas 
+e cadeias em um aplicativo LLM, permitindo que seu LLM decida sobre a próxima ferramenta de entrada 
+a ser usada com base na entrada do usuário.
+
+Você deve instalar os seguintes pacotes:
+
+$ !pip -q install langchain huggingface_hub openai google-search-results tiktoken wikipedia
 """
 
 # Isto é quando usas o arquivo .env:
@@ -37,11 +49,57 @@ tools = load_tools(["serpapi", "llm-math"],
 
 
 # O que é uma ferramenta como esta:
-print(tools[1].name, tools[1].description)
+#print(tools[1].name, tools[1].description)
 
-print(tools[1])
+#print(tools[1])
+
+"""
+Inicializando o agente
+======================
+"""
+agent = initialize_agent(tools, 
+                         llm, 
+                         agent="zero-shot-react-description", 
+                         verbose=False)
+
+agent.agent.llm_chain.prompt.template
 
 
+# Consulta LLM padrão
+#print(agent.run("Olá, como você está hoje?"))
 
 
+print(agent.run("Quem é o presidente dos Estados Unidos?"))
 
+
+"""
+Novo agente
+===========
+"""
+tools = load_tools(["serpapi", "llm-math", "wikipedia", "terminal"],
+                   llm=llm
+                  )
+
+
+agent = initialize_agent(tools, 
+                         llm, 
+                         agent="zero-shot-react-description", 
+                         verbose=False
+                        )
+
+
+agent.agent.llm_chain.prompt.template
+
+
+# print(agent.run("Quem é o chefe da DeepMind?"))
+
+
+# print(agent.run("O que é DeepMind?"))
+
+
+# print(agent.run("Se eu elevar ao quadrado o número do endereço da DeepMind, que resposta obtenho?"))
+
+
+# print(agent.run("Quais arquivos estão no meu diretório atual?"))
+
+print(agent.run("Meu diretório atual tem um arquivo sobre a Califórnia?"))
